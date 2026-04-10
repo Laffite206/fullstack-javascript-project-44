0aa575c747f0d901bfabe970155258517c07d481
Laffite206 <Lehaarapan10@gmail.com>
2026-04-10 22:06:22 +0700

    Вторая Игра

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Laffite206/fullstack-javascript-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Laffite206/fullstack-javascript-project-44/actions)
+
+### Демонстрация игры (Проверка на чётность)
+https://asciinema.org/a/tFxYpJiQmJfyrHAt
---
+++ src/games/calc.js
@@ -0,0 +1,21 @@
+import { runGame } from '../engine.js'
+
+const makeQuestion = () => {
+  const expression = ['+', '-', '*']
+  const randomNum1 = Math.floor(Math.random() * 50)
+  const randomNum2 = Math.floor(Math.random() * 55)
+  const randomOperation = expression[Math.floor(Math.random() * expression.length)]
+  let correctAnswer
+  if (randomOperation === '+') {
+    correctAnswer = randomNum1 + randomNum2
+  }
+  else if (randomOperation === '-') {
+    correctAnswer = randomNum1 - randomNum2
+  }
+  else if (randomOperation === '*') {
+    correctAnswer = randomNum1 * randomNum2
+  }
+  return { question: `${randomNum1} ${randomOperation} ${randomNum2}`, correctAnswer: String(correctAnswer)}
+};
+
+export default () => runGame('What is the result of the expression?', makeQuestion)
